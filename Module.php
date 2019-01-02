c0407af5c4c97e164632e02289bbfa2732008632
alex-wdmg <wdmg.com.ua@gmail.com>
2019-01-02 21:20:28 +0200

    Added module files.

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace wdmg\tasks;
+
+/**
+ * tasks module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'wdmg\tasks\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
