94fbc7d95a865da754d4de142f33ab68507ecb2a
alex-wdmg <wdmg.com.ua@gmail.com>
2019-01-26 22:03:49 +0200

    Added access control based on roles

--- controllers/ListController.php
+++ controllers/ListController.php
@@ -26,7 +26,7 @@ class ListController extends Controller
      */
     public function behaviors()
     {
-        return [
+        $behaviors = [
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
@@ -37,12 +37,27 @@ class ListController extends Controller
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'allow' => true,
-                        'roles' => ['@']
+                        'roles' => ['admin'],
+                        'allow' => true
                     ],
                 ],
             ],
         ];
+
+        // If auth manager not configured use default access control
+        if(!Yii::$app->authManager) {
+            $behaviors['access'] = [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'roles' => ['@'],
+                        'allow' => true
+                    ],
+                ]
+            ];
+        }
+
+        return $behaviors;
     }
 
     /**
